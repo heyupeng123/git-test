535b252ef6a016b0e33856be575468c0fc56312f
youmi <2848954798@qq.com>
2021-10-29 19:18:35 +0800

    second time commit!!!

--- src/main/java/com/itheima/springcloud/hello.java
+++ src/main/java/com/itheima/springcloud/hello.java
@@ -3,5 +3,6 @@ package com.itheima.springcloud;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("I love you");
     }
 }
